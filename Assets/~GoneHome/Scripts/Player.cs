a8fd3e372fde185636e9158b55794e0227fe343f
LmaoItsJaden <jaden.cheung6520@gmail.com>
2018-04-10 18:03:22 +1000

    Started programming Player

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
- * */
+[] - Brackets 
+{} - Braces
+() - Parenthesis 
+
+Hot kets:
+- Clean Codes: CTRL + K + D 
+- Fold code: CTRL + M + O 
+- unfold Code: CTRL + M + P 
+*/
 
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public string message = "Hello World!";
+    public Rigidbody rigid;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+
+
+    }
 }
